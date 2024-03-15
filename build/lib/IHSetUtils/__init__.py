adb59979e60513c7abeeb0f5ab21458caa8b5226
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2024-03-15 11:37:52 +0100

    First one

---
+++ build/lib/IHSetUtils/__init__.py
@@ -0,0 +1,4 @@
+# src/__init__.py
+
+# Import modules and functions from your package here
+from .jaramillo20 import jaramillo20, spt_setup_NSGAII
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='IHSetJaramillo20',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'numpy',
+        'scipy',
+        'xarray',
+        'numba',
+        'datetime'
+    ],
+    author='Lucas de Freitas Pereira',
+    author_email='lucas.defreitas@unican.es',
+    description='IH-SET Jaramillo et al. (2020)',
+    url='https://github.com/defreitasL/IHSetJaramillo20',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
