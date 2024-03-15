dc21240434e562dfa292fa6131f0c57bad5b1a2b
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2024-03-15 12:41:13 +0100

    First one

--- build/lib/IHSetJaramillo20/calibration.py
+++ build/lib/IHSetJaramillo20/calibration.py
@@ -44,7 +44,7 @@ class cal_Jaramillo20(object):
         self.time = mkTime(wav['Y'].values, wav['M'].values, wav['D'].values, wav['h'].values)
         self.E = self.Hs ** 2
 
-        self.Y_obs = ens['Yobs'].values
+        self.Y_obs = ens['Obs'].values
         self.time_obs = mkTime(ens['Y'].values, ens['M'].values, ens['D'].values, ens['h'].values)
 
         if self.switch_Yini == 0:
